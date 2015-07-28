0f8ea753ce3c11e57d0390dda0d51203840f30ba
Saif Syed <saif.syed@orionhealth.com>
2015-07-28 22:15:44 +1200

    Simplified the install

--- installmongo.sh
+++ installmongo.sh
@@ -5,15 +5,9 @@ echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.
 apt-get -y update
 apt-get install -y mongodb-org
 
-echo 'Stop Mongo'
-service mongod stop
-
-echo 'Making directories'
-mkdir /home/vagrant/mongo
-mkdir /home/vagrant/mongo/data
-mkdir /home/vagrant/mongo/log
-
-echo 'Starting mongo with changed conf'
-mongod --config /mongo/mongod.conf &
+echo 'Copying Conf'
+cp /mongo/mongod.conf /etc/mongod.conf
+echo 'Restarting mongo with changed conf'
+service mongod restart
 echo 'Mongo Started'
 
